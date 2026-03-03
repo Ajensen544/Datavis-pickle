844f473480e547cd6e6f2a56d3bb7868098ce230
jacobaburns54 <jacobaburns54@users.noreply.github.com>
2026-03-03 12:08:23 +0000

    deploy: b8ec65288e23dbcdc92b0578db09558ad64f92b9

---
+++ PR2/basic-questionnaire-study/assets/trialComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrialComponent = () => {
+    return (
+        <div>
+            <h2>Trial Component</h2>
+            <p>This is a basic React component.</p>
+        </div>
+    );
+};
+
+export default TrialComponent;
